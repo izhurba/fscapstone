e287ad827953d1cf015c15edf79f63c89aadef6d
Ilya Zhurba <i.n.zhurba@gmail.com>
2022-05-14 22:45:38 -0600

    Added models, auth, and gitignore

--- models.py
+++ models.py
@@ -22,22 +22,102 @@ def setup_db(app, database_path=database_path):
 
 
 '''
-Person
-Have title and release year
+Field Technician
 '''
-class Person(db.Model):  
-  __tablename__ = 'People'
+class fieldTech(db.Model):  
+  __tablename__ = 'Technicians'
 
-  id = Column(db.Integer, primary_key=True)
-  name = Column(db.String)
-  catchphrase = Column(db.String)
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, nullable=False)
+  employeeID = db.Column(db.Integer, nullable=False)
+  lead = db.relationship('leadTech', backref='fieldtech', lazy='joined', cascade='all, delete')
 
-  def __init__(self, name, catchphrase=""):
+
+  def __init__(self, name, employeeID):
     self.name = name
-    self.catchphrase = catchphrase
+    self.employeeID = employeeID
 
   def format(self):
     return {
       'id': self.id,
       'name': self.name,
-      'catchphrase': self.catchphrase}
+      'employeeID': self.employeeID}
+
+  def insert(self):
+      db.session.add(self)
+      db.session.commit()
+
+  def delete(self):
+      db.session.delete(self)
+      db.session.commit()
+
+  def update(self):
+      db.session.commit()
+
+class leadTech(db.Model):  
+  __tablename__ = 'Leads'
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, nullable=False)
+  employeeID = db.Column(db.Integer, nullable=False)
+  fieldtech_ids = db.Column(db.Array(db.Integer), db.ForeignKey(fieldTech.id), nullable=False)
+  senior = db.relationship('seniorTech', backref='leadtech', lazy='joined', cascade='all, delete')
+
+
+  def __init__(self, name, employeeID, fieldtech_ids):
+    self.name = name
+    self.employeeID = employeeID
+    self.fieldtech_ids = fieldtech_ids
+
+  def format(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'employeeID': self.employeeID,
+      'fieldtech_ids': self.fieldtech_ids}
+
+  def insert(self):
+      db.session.add(self)
+      db.session.commit()
+
+  def delete(self):
+      db.session.delete(self)
+      db.session.commit()
+
+  def update(self):
+      db.session.commit()
+
+class seniorTech(db.Model):  
+  __tablename__ = 'Seniors'
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, nullable=False)
+  employeeID = db.Column(db.Integer, nullable=False)
+  fieldtech_ids = db.Column(db.Array(db.Integer), db.ForeignKey(fieldTech.id), nullable=False)
+  leadtech_ids = db.Column(db.Array(db.Integer), db.ForeignKey(leadTech.id), nullable=False)
+
+
+  def __init__(self, name, employeeID, fieldtech_ids, leadtech_ids):
+    self.name = name
+    self.employeeID = employeeID
+    self.fieldtech_ids = fieldtech_ids
+    self.leadtech_ids = leadtech_ids
+
+  def format(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'employeeID': self.employeeID,
+      'fieldtech_ids': self.fieldtech_ids,
+      'leadtech_ids': self.leadtech_ids}
+
+  def insert(self):
+      db.session.add(self)
+      db.session.commit()
+
+  def delete(self):
+      db.session.delete(self)
+      db.session.commit()
+
+  def update(self):
+      db.session.commit()
